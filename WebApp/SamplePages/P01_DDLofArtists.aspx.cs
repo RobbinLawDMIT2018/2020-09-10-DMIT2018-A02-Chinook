af87e4b4bff457e21e61e4860e3cc18cce131a6c
robbinl <robbinl@telusplanet.net>
2020-09-15 14:18:08 -0600

    Finished PL tags and code behind

--- WebApp/SamplePages/P01_DDLofArtists.aspx.cs
+++ WebApp/SamplePages/P01_DDLofArtists.aspx.cs
@@ -13,5 +13,28 @@ namespace WebApp.SamplePages
         {
 
         }
+        protected void DisplaySelectedInfo1_Click(object sender, EventArgs e)
+        {
+            {
+                var selectedindex = DDLofArtists1.SelectedIndex;
+                var selectedvalue = DDLofArtists1.SelectedValue;
+                var selecteditem = DDLofArtists1.SelectedItem;
+                MessageLabel.Text = $"SelectedIndex:{selectedindex} " +
+                                    $" SelectedValue:{selectedvalue} " +
+                                    $" SelectedItem:{selecteditem}";
+            }
+        }
+
+        protected void DisplaySelectedInfo2_Click(object sender, EventArgs e)
+        {
+            {
+                var selectedindex = DDLofArtists2.SelectedIndex;
+                var selectedvalue = DDLofArtists2.SelectedValue;
+                var selecteditem = DDLofArtists2.SelectedItem;
+                MessageLabel.Text = $"SelectedIndex:{selectedindex} " +
+                                    $" SelectedValue:{selectedvalue} " +
+                                    $" SelectedItem:{selecteditem}";
+            }
+        }
     }
 }
