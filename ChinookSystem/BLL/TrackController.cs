bbb1cff3192974ea758938fa27f460f6cd4f7bb5
robbinl <robbinl@telusplanet.net>
2020-09-17 13:40:46 -0600

    Built Tack controller

---
+++ ChinookSystem/BLL/TrackController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Additional Namespaces
+using System.ComponentModel;
+using ChinookSystem.DAL;
+using ChinookSystem.VIEWMODELS;
+
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class TrackController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<TrackViewModel> Track_List()
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                var results = from x in context.Tracks
+                              select new TrackViewModel
+                              {
+                                  TrackName = x.Name,
+                                  TrackComposer = x.Composer,
+                                  TrackMilliseconds = x.Milliseconds,
+                                  TrackBytes = x.Bytes,
+                                  TrackUnitPrice = x.UnitPrice
+                              };
+                return results.ToList();
+            }
+        }
+    }
+}
