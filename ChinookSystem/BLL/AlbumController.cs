32c7153f7677b19ceb6c9746295f42e9eda5408a
robbinl <robbinl@telusplanet.net>
2020-09-23 08:36:29 -0600

    Created Album insert, update, delete methods in AlbumController BUT with NO VALIDATION.

--- ChinookSystem/BLL/AlbumController.cs
+++ ChinookSystem/BLL/AlbumController.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using System.ComponentModel;
 using ChinookSystem.DAL;
 using ChinookSystem.VIEWMODELS;
+using ChinookSystem.ENTITIES;
 
 namespace ChinookSystem.BLL
 {
@@ -48,6 +49,66 @@ namespace ChinookSystem.BLL
                                   AlbumReleaseLabel = x.ReleaseLabel
                               };
                 return results.ToList();
+            }
+        }
+        #endregion
+
+        #region Insert,Update,Delete
+
+        //REMEMBER to add the DataKeyNames="AlbumId" 
+        //attribute to your ListView so that Delete will work
+
+        [DataObjectMethod(DataObjectMethodType.Insert, false)]
+        public void Albums_Insert(AlbumViewModel item)
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                Album info = new Album()
+                {
+                    Title = item.AlbumTitle,
+                    ArtistId = item.ArtistId,
+                    ReleaseYear = item.AlbumReleaseYear,
+                    ReleaseLabel = item.AlbumReleaseLabel
+                };
+                context.Albums.Add(info);
+                context.SaveChanges();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Update, false)]
+        public void Albums_Update(AlbumViewModel item)
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                Album info = new Album()
+                {
+                    AlbumId = item.AlbumId,
+                    Title = item.AlbumTitle,
+                    ArtistId = item.ArtistId,
+                    ReleaseYear = item.AlbumReleaseYear,
+                    ReleaseLabel = item.AlbumReleaseLabel
+                };
+                context.Entry(info).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Delete, false)]
+        public void Albums_Delete(AlbumViewModel item)
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                Albums_Delete(item.AlbumId);
+            }
+        }
+
+        public void Albums_Delete(int albumid)
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                var existing = context.Albums.Find(albumid);
+                context.Albums.Remove(existing);
+                context.SaveChanges();
             }
         }
         #endregion
