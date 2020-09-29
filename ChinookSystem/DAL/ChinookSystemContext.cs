ab7626d25072964e589da2a67a9bf2bd1f5d0ee6
robbinl <robbinl@telusplanet.net>
2020-09-29 14:20:12 -0600

    Completed Reverse Engineering of all Entites and context and we tested it and it worked.

---
+++ ChinookSystem/DAL/ChinookSystemContext.cs
@@ -0,0 +1,119 @@
+namespace ChinookSystem.DAL
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using ChinookSystem.ENTITIES;
+
+    public partial class ChinookSystemContext : DbContext
+    {
+        public ChinookSystemContext()
+            : base("name=ChinookDB")
+        {
+        }
+
+        public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
+        public virtual DbSet<C__RefactorLog> C__RefactorLog { get; set; }
+        public virtual DbSet<Album> Albums { get; set; }
+        public virtual DbSet<Artist> Artists { get; set; }
+        public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
+        public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
+        public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
+        public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
+        public virtual DbSet<Genre> Genres { get; set; }
+        public virtual DbSet<InvoiceLine> InvoiceLines { get; set; }
+        public virtual DbSet<Invoice> Invoices { get; set; }
+        public virtual DbSet<MediaType> MediaTypes { get; set; }
+        public virtual DbSet<Playlist> Playlists { get; set; }
+        public virtual DbSet<PlaylistTrack> PlaylistTracks { get; set; }
+        public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
+        public virtual DbSet<Track> Tracks { get; set; }
+        public virtual DbSet<WorkingVersion> WorkingVersions { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Album>()
+                .Property(e => e.ReleaseLabel)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Artist>()
+                .HasMany(e => e.Albums)
+                .WithRequired(e => e.Artist)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<AspNetRole>()
+                .HasMany(e => e.AspNetUsers)
+                .WithMany(e => e.AspNetRoles)
+                .Map(m => m.ToTable("AspNetUserRoles").MapLeftKey("RoleId").MapRightKey("UserId"));
+
+            modelBuilder.Entity<AspNetUser>()
+                .HasMany(e => e.AspNetUserClaims)
+                .WithRequired(e => e.AspNetUser)
+                .HasForeignKey(e => e.UserId);
+
+            modelBuilder.Entity<AspNetUser>()
+                .HasMany(e => e.AspNetUserLogins)
+                .WithRequired(e => e.AspNetUser)
+                .HasForeignKey(e => e.UserId);
+
+            modelBuilder.Entity<Customer>()
+                .HasMany(e => e.Invoices)
+                .WithRequired(e => e.Customer)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Employee>()
+                .HasMany(e => e.Customers)
+                .WithOptional(e => e.Employee)
+                .HasForeignKey(e => e.SupportRepId);
+
+            modelBuilder.Entity<Employee>()
+                .HasMany(e => e.Employees1)
+                .WithOptional(e => e.Employee1)
+                .HasForeignKey(e => e.ReportsTo);
+
+            modelBuilder.Entity<InvoiceLine>()
+                .Property(e => e.UnitPrice)
+                .HasPrecision(10, 2);
+
+            modelBuilder.Entity<Invoice>()
+                .Property(e => e.Total)
+                .HasPrecision(10, 2);
+
+            modelBuilder.Entity<Invoice>()
+                .HasMany(e => e.InvoiceLines)
+                .WithRequired(e => e.Invoice)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<MediaType>()
+                .HasMany(e => e.Tracks)
+                .WithRequired(e => e.MediaType)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Playlist>()
+                .HasMany(e => e.PlaylistTracks)
+                .WithRequired(e => e.Playlist)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Track>()
+                .Property(e => e.UnitPrice)
+                .HasPrecision(10, 2);
+
+            modelBuilder.Entity<Track>()
+                .HasMany(e => e.InvoiceLines)
+                .WithRequired(e => e.Track)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Track>()
+                .HasMany(e => e.PlaylistTracks)
+                .WithRequired(e => e.Track)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<WorkingVersion>()
+                .Property(e => e.Comments)
+                .IsFixedLength();
+        }
+    }
+}
